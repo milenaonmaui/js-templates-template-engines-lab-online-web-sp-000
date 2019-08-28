730108b37503698d599b422111862ce42f11d34e
Milena Kari <milenakari@yahoo.com>
2019-08-28 01:26:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
 function createPost() {
-  event.preventDefault();
-  let title = document.getElementById('postTitle').value;
-  let body = document.getElementById('postBody').value;
-  let author = document.getElementById('postAuthor').value
-  let postTemplate = document.getElementById('post-template').innerHTML;
- 
-  //create template function
-  let templateFn = _.template(postTemplate);
- 
-  let postDiv = document.getElementById('posts');
- 
-  //execute template function with JSON object for the interpolated values
-  let templateHTML = templateFn({ title: title, body: body, author: author });
- 
-  //append rather than replace!
-  postDiv.innerHTML += templateHTML;
+  // create template functions
+  var pageTemplate = _.template(document.getElementById("page-template").innerHTML);
+  var postTemplate = _.template(document.getElementById("post-template").innerHTML);
+  var commentsTemplate = _.template(document.getElementById("comments-template").innerHTML);
+
+  // get blog values
+  var postTitle = document.getElementById("postTitle").value;
+  var postAuthor = document.getElementById("postAuthor").value;
+  var post = document.getElementById("postBody").value;
+
+  document.getElementsByTagName("main")[0].innerHTML += pageTemplate();
+
+  var blogSection = postTemplate({ 'title': postTitle, 'body': post, 'poster': postAuthor });
+  var commentsSection = commentsTemplate();
+  var postElement = document.getElementById("post");
+
+  postElement.innerHTML = blogSection;
+  postElement.getElementsByTagName("footer")[0].innerHTML = commentsSection;
+}
+
+function postComment() {
+  var commentTemplate = _.template(document.getElementById("comment-template").innerHTML);
+
+  var commentText = document.getElementById("commentText").value;
+  var commenterName = document.getElementById("commenter").value;
+
+  var commentsSection = document.getElementById("comments");
+  commentsSection.innerHTML += commentTemplate({ 'commenter': commenterName, 'comment': commentText });
 }
